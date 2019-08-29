db89ab20ff79ba50bbd392982a4a57dd2bb10ecc
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2019-08-29 16:42:38 +0200

    Suppress Tensorflow warnings

--- src/run.sh
+++ src/run.sh
@@ -3,8 +3,9 @@
 # Run (Python) script with correct PYTHONPATH.
 #
 
-PYTHONPATH="$(dirname "$(realpath "$0")"):$PYTHONPATH"
-export PYTHONPATH
+export PYTHONPATH="$(dirname "$(realpath "$0")"):$PYTHONPATH"
+export TF_CPP_MIN_LOG_LEVEL=3
+export PYTHONWARNINGS=ignore
 
 if [ -x "$1" ]; then
     exec "$@"
